fc89eedae37d0e262a0e3e3a08234a0509691d56
Helaly1804 <elhelaly1804@gmail.com>
2025-04-17 15:06:27 +0200

    Department Controller Index

---
+++ Demo.Presentation/Controllers/DepartmentController.cs
@@ -0,0 +1,14 @@
+﻿using Demo.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class DepartmentController(IDepartmentService departmentService) : Controller
+    {
+        public IActionResult Index()
+        {
+            var departments = departmentService.GetAllDepartments();
+            return View(departments);
+        }
+    }
+}
